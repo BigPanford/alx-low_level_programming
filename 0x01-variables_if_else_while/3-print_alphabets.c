345d23a140fdf7ee2064f506dee1230c0f5bb916
BigPanford <cycasbbone@gmail.com>
2023-06-17 15:16:07 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+/**
+ * main - Starting point
+ * Description: 'Write a program that prints the alphabet in lower and upper'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int d = 97;
+	int e = 65;
+
+	while (d <= 122)
+	{
+		putchar(d);
+	}
+	while (e <= 90)
+	{
+		putchar(e);
+	}
+	putchar('\n');
+	return (0);
+}
