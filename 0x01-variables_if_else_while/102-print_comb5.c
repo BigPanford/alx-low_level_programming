1a49506642ab7f8b5706a2d75bc72ad3bb2dbb68
BigPanford <cycasbbone@gmail.com>
2023-06-18 13:38:12 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -19,7 +19,7 @@ int main(void)
 			putchar((b / 10) + '0');
 			putchar((b % 10) + '0');
 
-				if (a == 99 && b == 98)
+				if (a == 98 && b == 99)
 					continue;
 				putchar(',');
 				putchar(' ');
