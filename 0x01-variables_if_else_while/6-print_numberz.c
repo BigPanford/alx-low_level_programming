426e1fc2bd563f3a06022ef2d66fe3ff57e82ac4
BigPanford <cycasbbone@gmail.com>
2023-06-17 17:42:53 -0700

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,4 +1,17 @@
-#include<stdio>
+#include<stdio.h>
 /**
  * main - Starting point
- * Description:' Write a program that prints all single digit numbers of base 10'
+ * Description:' Write a program that prints all single digit numbers'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int d;
+
+	for (d = 0; d <= 9; d++)
+	{
+		putchar(d + 48);
+	}
+	putchar('\n');
+	return (0);
+}
