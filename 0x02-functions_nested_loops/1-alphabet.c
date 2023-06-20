3beb2bff1ff6498c5951bfe0cdb1c72ae34099ee
BigPanford <cycasbbone@gmail.com>
2023-06-20 14:16:36 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_aphabet - mMake the alphabet
+ * Retuen: void
+ */
+void print_alphabet(void)
+{
+	char d;
+
+	for (d = '0'; d <= 'z'; d++)
+	{
+		_putchar(d);
+	}
+	_putchar('\n');
+}
+
