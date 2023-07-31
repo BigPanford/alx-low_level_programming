e5d036e50cb0168080fc76224cef931a1225e83c
BigPanford <cycasbbone@gmail.com>
2023-07-31 06:36:21 -0700

    4-free_listint.c

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_listint - Frees the linked list
+ * @head: listint_t list to be freed
+ * Return: Nothing
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+
+	while (head)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
+}
